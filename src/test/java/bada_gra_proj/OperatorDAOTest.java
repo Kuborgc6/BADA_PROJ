29aed29017c7b23a46028649c52286065b4d7dbc
Jakub Ciborowski <cibor.kuba@gmail.com>
2021-01-15 13:15:31 +0100

    zmiany w testach

--- src/test/java/bada_gra_proj/OperatorDAOTest.java
+++ src/test/java/bada_gra_proj/OperatorDAOTest.java
@@ -12,16 +12,16 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 class OperatorDAOTest {
 
 	private OperatorDAO dao;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
 		DriverManagerDataSource datasource = new DriverManagerDataSource();
 		datasource.setUrl("jdbc:oracle:thin:@localhost:1521:ORCL6");
-		/*datasource.setUrl("jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:ora3inf");*/
+		/* datasource.setUrl("jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:ora3inf"); */
 		datasource.setUsername("system");
 		datasource.setPassword("dupa1234");
 		datasource.setDriverClassName("oracle.jdbc.OracleDriver");
-		
+
 		/* Import JdbcTemplate */
 		dao = new OperatorDAO(new JdbcTemplate(datasource));
 	}
@@ -29,13 +29,14 @@ class OperatorDAOTest {
 	@Test
 	void testList() {
 		List<Operator> listOperator = dao.list();
-		assertTrue(listOperator.isEmpty());
+		assertFalse(listOperator.isEmpty());
 	}
 
 	@Test
 	void testSave() {
-		fail("Not yet implemented");
-	}
+		Operator operator = new Operator(0,"GoodYear NewOne", "2012-06-05","Polska", "420692137");
+		dao.save(operator)
+;	}
 
 	@Test
 	void testGet() {
